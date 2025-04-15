56f6a4d98ffe738e83d1920c968e81e773fc817e
Eli <9064062+snood1205@users.noreply.github.com>
2025-04-15 09:28:46 -0400

    Finished with app.bsky.* initial passthrough

--- lib/arroba/bsky/video.rb
+++ lib/arroba/bsky/video.rb
@@ -3,6 +3,12 @@
 module Arroba
   class BSky
     class Video < BaseResource
+      get_with_query_params :get_job_status, :job_id
+      basic_get :get_upload_limits
+
+      def upload_video!(mp4_blob:)
+        post body: mp4_blob
+      end
     end
   end
 end
