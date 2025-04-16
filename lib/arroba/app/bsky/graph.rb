eda3186ebe7b14d9633de3293aba33f16f0d6eec
Eli <9064062+snood1205@users.noreply.github.com>
2025-04-16 12:14:36 -0400

    Cleanup

--- lib/arroba/app/bsky/graph.rb
+++ lib/arroba/app/bsky/graph.rb
@@ -8,11 +8,6 @@ module Arroba
       # The Graph resource provides access to the social graph of a user, including followers, follows, and blocks.
       class Graph < BaseResource
         include Validations::Limitable
-        # Document the dynamically defined method as a placeholder
-        #
-        # @method get_actor_starter_packs
-        # This method is defined dynamically.
-        # @return [Integer] The result of the addition (always 6 in this case).
         get_with_enforced_limit :get_actor_starter_packs, :actor
         get_with_enforced_limit :get_blocks
         get_with_enforced_limit :get_followers, :actor
